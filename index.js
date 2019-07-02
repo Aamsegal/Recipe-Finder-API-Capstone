e84ff1bae061e03d88c7c2e39c9c34c63112814a
Amsegal93 <Amsegal93@gmail.com>
2019-07-02 17:03:50 -0400

    Fixed bugs when accessing object keys that dont exist. Added back and next button. And added nutrient info.

--- index.js
+++ index.js
@@ -14,14 +14,54 @@ function formatQueryParams(params) {
   return queryItems.join('&');
 }
 
+function navigationButtons(foodTerm, alergyBooleanArray, fromVariable, toVariable) {
+  /*$('#navigation-buttons').submit(event => {
+    event.preventDefault();
+    fromVariable = fromVariable + 10;
+    toVariable = toVariable + 10;
+    getInitialRecipeList(foodTerm, alergyBooleanArray, fromVariable, toVariable);*/
 
-function getInitialRecipeList(foodTerm, cuisineTerm, alergyBooleanArray, callNumber) {
+    document.getElementById('back-button').onclick = function() {
+      event.preventDefault();
+      fromVariable = fromVariable - 10;
+      toVariable = toVariable - 10;
+      getInitialRecipeList(foodTerm, alergyBooleanArray, fromVariable, toVariable);
+      window.location = '#Home';
+    };
+
+    document.getElementById('next-button').onclick = function() {
+      event.preventDefault();
+      fromVariable = fromVariable + 10;
+      toVariable = toVariable + 10;
+      getInitialRecipeList(foodTerm, alergyBooleanArray, fromVariable, toVariable);
+      window.location = '#Home';
+    }
+};
+
+/*function nextButtonPressed (foodTerm, alergyBooleanArray, fromVariable, toVariable) {
+  fromVariable = fromVariable - 10;
+  toVariable = toVariable - 10;
+  getInitialRecipeList(foodTerm, alergyBooleanArray, fromVariable, toVariable);
+}
+
+function backButtonPressed (foodTerm, alergyBooleanArray, fromVariable, toVariable) {
+  fromVariable = fromVariable - 10;
+  toVariable = toVariable - 10;
+  getInitialRecipeList(foodTerm, alergyBooleanArray, fromVariable, toVariable);
+}*/
+
+function getInitialRecipeList(foodTerm, alergyBooleanArray, fromVariable, toVariable) {
+  if (fromVariable > 0) {
+    document.getElementById('back-button').style.display = 'block';
+    document.getElementById('next-button').style.display = 'inline-block';
+  } else {
+    document.getElementById('back-button').style.display = 'none';
+    document.getElementById('next-button').style.display = 'inline-block';
+  }
   const params = {
     q: foodTerm,
     app_id: apiId,
-    app_key: apiKey,
-    from: 0,
-    to: 10
+    app_key: apiKey
     };
 
     /*const fromBase = 0;
@@ -30,12 +70,14 @@ function getInitialRecipeList(foodTerm, cuisineTerm, alergyBooleanArray, callNum
     const to = toBase + callNumber*10;*/
     // the code above is trying to set the ground work to scroll for more recipies
   const queryString = formatQueryParams(params);
-  let url = searchURL + '?' + queryString /*+ `&from=${from}&to=${to}`*/;
+  let url = searchURL + '?' + queryString + `&from=${fromVariable}&to=${toVariable}`;
   for (let alergyIndex = 0; alergyIndex <= 15; alergyIndex++) {
     if (alergyBooleanArray[alergyIndex]) {
         url =  url + '&health='+ $(`#alergy${alergyIndex}`).val();
     } 
-  } 
+  }
+
+  /*fromToUrl = nextBack();*/
 
 
   console.log(url);
@@ -56,6 +98,9 @@ fetch(url)
 });
 }
 
+function getDailyNutrients(path,ingredient,defaultValue = 0) {
+  return R.path(path,ingredient) || defaultValue;
+}
 
 function displayResults(responseJson, url) {
   // if there are previous results, remove them
@@ -77,14 +122,58 @@ function displayResults(responseJson, url) {
   ).join('');*/
   const jsonLength = responseJson.hits.length;
   for (let i = 0; i < jsonLength; i++) {
+
     const jsonIndex = responseJson.hits[i];
+    const servingSize = jsonIndex.recipe.yield;
+
+    let caloriesPerServing = jsonIndex.recipe.calories/servingSize;
+    caloriesPerServing = Math.round(caloriesPerServing);
+
+    let dailyFat = getDailyNutrients(['FAT','quantity'],jsonIndex.recipe.totalDaily)/servingSize;
+    dailyFat = Math.round(dailyFat);
+
+    let dailyFiber = getDailyNutrients(['FIBTG','quantity'],jsonIndex.recipe.totalDaily)/servingSize;
+    dailyFiber = Math.round(dailyFiber)
+
+    let dailyProtein = getDailyNutrients(['PROCNT','quantity'],jsonIndex.recipe.totalDaily)/servingSize;
+    dailyProtein = Math.round(dailyProtein);
+
+    let dailyCholesterol = getDailyNutrients(['CHLOE','quantity'],jsonIndex.recipe.totalDaily)/servingSize;
+    dailyCholesterol = Math.round(dailyCholesterol);
+
+    let dailySodium = getDailyNutrients(['NA','quantity'],jsonIndex.recipe.totalDaily)/servingSize;
+    dailySodium = Math.round(dailySodium);
+
+    let dailyCalcium = getDailyNutrients(['CA','quantity'],jsonIndex.recipe.totalDaily)/servingSize;
+    dailyCalcium = Math.round(dailyCalcium);
+
+    let dailyIron = getDailyNutrients(['FE','quantity'],jsonIndex.recipe.totalDaily)/servingSize;
+    dailyIron = Math.round(dailyIron);
+
+    let dailyCarbs = getDailyNutrients(['CHOCDF','quantity'],jsonIndex.recipe.totalDaily)/servingSize;
+    dailyCarbs = Math.round(dailyCarbs);
+
+
+
 
     
     $('#recipe-results').append(
       `<section class="recipe-section ${i}">
-      <h2>${jsonIndex.recipe.label} (${jsonIndex.recipe.yield} servings)</h2>
+      <h2>${jsonIndex.recipe.label} (${servingSize} servings)</h2>
       <img src="${jsonIndex.recipe.image}" class="food-image">
       <ul id="recipe-${i}"></ul>
+      
+      <h2>Nutrients</h2>
+      <ul class="nutrient-list">
+      <li>${caloriesPerServing} calories per serving</li>
+      <li>${dailyFat}% of your daily Fat</li>
+      <li>${dailyFiber}% of your daily Fiber</li>
+      <li>${dailyProtein}% of your daily Protein</li>
+      <li>${dailyCholesterol}% of your daily Cholesterol</li>
+      <li>${dailySodium}% of your daily Sodium</li>
+      <li>${dailyCalcium}% of your daily Calcium</li>
+      <li>${dailyIron}% of your daily Iron</li>
+      <li>${dailyCarbs}% of your daily Carbs</li></ul>
       <a href=${jsonIndex.recipe.url} target="_blank">Find the cooking instructions here</a><br>
       </section>
       `
@@ -103,21 +192,34 @@ function getIngredients(responseJson,i) {
     }
 }
 
-function watchForm() {
+
+
+/*function getNutrients(keys,recipt) {
+  for (let i = 0; i < keys.length; i++) {
+    if (!recipt[keys[i]]) {
+      return;
+    }
+  }
+}*/
+
+function watchForm(fromVariable, toVariable) {
   $('#food-form').submit(event => {
     event.preventDefault();
     const foodTerm = $('#js-food-term').val();
-    const cuisineTerm = $('#js-cuisine-term').val();
     const alergyBooleanArray = [];
     let alergyBoolean = true;
     for (let a = 0; a <= 15; a++) {
         alergyBoolean = $(`#alergy${a}`).is(':checked');
         alergyBooleanArray.push(alergyBoolean);
     }
-    const initialCall = 0;
     console.log(alergyBooleanArray);
-    getInitialRecipeList(foodTerm, cuisineTerm, alergyBooleanArray, initialCall);
+    document.getElementById('next-button').style.display = 'block';
+    getInitialRecipeList(foodTerm, alergyBooleanArray, fromVariable, toVariable);
+    navigationButtons(foodTerm, alergyBooleanArray, fromVariable, toVariable);
+
+    
+    
   });
 }
 
-$(watchForm);
+$(watchForm(0,9));
