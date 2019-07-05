5696d72b14a7193bc807fbcc12a87db2ba198b6e
Amsegal93 <Amsegal93@gmail.com>
2019-07-05 16:57:24 -0400

    Fixed HTML issues. Improved JS architecture and improved code efficiency. Spell checked work

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 App name: Recipe Finder
 
-Live app link: https://aamsegal.github.io/Recipe-Finder-API-Capstone/
+Live app link: HTTPS://aamsegal.github.io/Recipe-Finder-API-Capstone/
 
-Summary: Users can type foods ranging from chicken to cookies into the search bar, choose dietary and health restrictions and the website will provide recipies
+Summary: Users can type foods ranging from chicken to cookies into the search bar, choose dietary and health restrictions and the website will provide recipes
 fitting those filters. The user can see a picture, nutrients and ingredients of the recipe as well as a link to further cooking details.
 
-Technolgys: HTML, CSS, JavaScript, Jquery, Ramda Path function (this checks to see if a path exists, and if it doesnt, set the value to a default value instead of
+Technology: HTML, CSS, JavaScript, Jquery, Ramda Path function (this checks to see if a path exists, and if it doesn't, set the value to a default value instead of
 crashing the program.
--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-'use strict';
-
-// put your own value below!
 const apiKey = '1d991e6283e9a6a365d439062706bb24';
 const apiId = '92de8f02';
 const searchURL = 'https://api.edamam.com/search';
-const exampleCall = 'https://api.edamam.com/search?q=chicken&app_id=92de8f02&app_key=1d991e6283e9a6a365d439062706bb24&from=0&to=3&calories=591-722&health=alcohol-free';
-const dietaryRestrictionList ="dairy-free   egg-free   gluten-free   keto-free   khosher   low-sugar   paleo   red-meat-free   seasame-free   shellfish-free   soy-free   vegan vegitarian"
-
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
   return queryItems.join('&');
 }
 //goes through the keys in the params object which are requited values for this API call, and creates the text for the url
+
+
+
 
 function navigationButtons(foodTerm, alergyBooleanArray, fromVariable, toVariable) {
 
@@ -38,25 +35,20 @@ function navigationButtons(foodTerm, alergyBooleanArray, fromVariable, toVariabl
     //when the next button is clicked, run the recipe api call with a higher from and to value
 };
 
-function getInitialRecipeList(foodTerm, alergyBooleanArray, fromVariable, toVariable) {
-  if (fromVariable > 0) {
-    document.getElementById('back-button').style.display = 'inline-block';
-    document.getElementById('next-button').style.display = 'inline-block';
-  } else {
-    document.getElementById('back-button').style.display = 'none';
-    document.getElementById('next-button').style.display = 'inline-block';
-  }
 
-  //shows the back and next buttons depending on the value of the from variable
+
+function getInitialRecipeList(foodTerm, alergyBooleanArray, fromVariable, toVariable) {
 
   const params = {
     q: foodTerm,
     app_id: apiId,
     app_key: apiKey
-    };
+  };
 
   const queryString = formatQueryParams(params);
+
   let url = searchURL + '?' + queryString + `&from=${fromVariable}&to=${toVariable}`;
+
   for (let alergyIndex = 0; alergyIndex <= 15; alergyIndex++) {
     if (alergyBooleanArray[alergyIndex]) {
         url =  url + $(`#alergy${alergyIndex}`).val();
@@ -64,33 +56,55 @@ function getInitialRecipeList(foodTerm, alergyBooleanArray, fromVariable, toVari
   }
   //creates the api url using the search url, query string, and values from the alergy checkboxes and back and next buttons
 
-  console.log(url);
+  fetch(url)
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson,fromVariable))
+    .catch(err => {
+        $('.recipe-results').empty();
+        $('#js-error-message').text(`Something went wrong: ${err.message}`);
 
-
-fetch(url)
-  .then(response => {
-      if (response.ok) {
-          return response.json();
-      }
-      throw new Error(response.statusText);
-  })
-  .then(responseJson => displayResults(responseJson))
-  .catch(err => {
-      $('.recipe-results').empty();
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
-
-  });
-  }
+    });
+}
   //standard API call. If the responce is acceptable, we get the JSON file. If the call is bad, then it returns an error message
 
-function getDailyNutrients(path,ingredient,defaultValue = 0) {
+
+
+function preventDailyNutrientsError(path,ingredient,defaultValue = 0) {
   return R.path(path,ingredient) || defaultValue;
 }
-/*uses ramda function to check if a value is defined, and if it isnt, return 0. This prevents the program from crashing if the value is undefined
-which it can be if a recipe doesnt have any of the called nutrients*/
 
-function displayResults(responseJson, url) {
-  console.log(responseJson);
+
+
+function getDailyNutrients(jsonIndex) {
+  
+  const nutrientArrayTags = ['FAT','FIBTG','PROCNT','CHLOE','NA','CA','FE','CHOCDF'];
+  //Above is an array of the HTML tags to filter for different dietary restrictions in the API call
+  
+  //FIBTG = fiber, PROCNT = protein, CHLOE = cholesterol, CHOCFG = carbs
+  const nutrientArray = [];
+
+  const servingSize = jsonIndex.recipe.yield;
+  nutrientArray.push(servingSize);
+
+  let caloriesPerServing = jsonIndex.recipe.calories/servingSize;
+  nutrientArray.push(Math.round(caloriesPerServing));
+
+  for (let i = 0 ; i < nutrientArrayTags.length; i++) {
+    let dailyNutrient = preventDailyNutrientsError([`${nutrientArrayTags[i]}`,'quantity'],jsonIndex.recipe.totalDaily)/servingSize;
+    nutrientArray.push(Math.round(dailyNutrient));
+    /*uses ramda function to check if a value is defined, and if it isnt, return 0. This prevents the program from crashing if the value is undefined
+    which it can be if a recipe doesnt have any of the called nutrients*/
+  }
+
+  return nutrientArray;
+}
+
+function displayResults(responseJson, fromVariable) {
   $('.recipe-results').empty();
   //clears the recipe results in order to post new ones
 
@@ -99,91 +113,74 @@ function displayResults(responseJson, url) {
     $('#recipe-results').append('No reslts found');
     return;
   }
+
+  if (fromVariable > 0) {
+    document.getElementById('back-button').style.display = 'inline-block';
+    document.getElementById('next-button').style.display = 'inline-block';
+  } else {
+    document.getElementById('back-button').style.display = 'none';
+    document.getElementById('next-button').style.display = 'inline-block';
+  }
+  //shows the back and next buttons depending on the value of the from variable
+  
   //if no results are found, return an error message saying no results were found.
 
   const jsonLength = responseJson.hits.length;
   for (let i = 0; i < jsonLength; i++) {
 
     const jsonIndex = responseJson.hits[i];
-    const servingSize = jsonIndex.recipe.yield;
 
-    let caloriesPerServing = jsonIndex.recipe.calories/servingSize;
-    caloriesPerServing = Math.round(caloriesPerServing);
-
-    let dailyFat = getDailyNutrients(['FAT','quantity'],jsonIndex.recipe.totalDaily)/servingSize;
-    dailyFat = Math.round(dailyFat);
-
-    let dailyFiber = getDailyNutrients(['FIBTG','quantity'],jsonIndex.recipe.totalDaily)/servingSize;
-    dailyFiber = Math.round(dailyFiber)
-
-    let dailyProtein = getDailyNutrients(['PROCNT','quantity'],jsonIndex.recipe.totalDaily)/servingSize;
-    dailyProtein = Math.round(dailyProtein);
-
-    let dailyCholesterol = getDailyNutrients(['CHLOE','quantity'],jsonIndex.recipe.totalDaily)/servingSize;
-    dailyCholesterol = Math.round(dailyCholesterol);
-
-    let dailySodium = getDailyNutrients(['NA','quantity'],jsonIndex.recipe.totalDaily)/servingSize;
-    dailySodium = Math.round(dailySodium);
-
-    let dailyCalcium = getDailyNutrients(['CA','quantity'],jsonIndex.recipe.totalDaily)/servingSize;
-    dailyCalcium = Math.round(dailyCalcium);
-
-    let dailyIron = getDailyNutrients(['FE','quantity'],jsonIndex.recipe.totalDaily)/servingSize;
-    dailyIron = Math.round(dailyIron);
-
-    let dailyCarbs = getDailyNutrients(['CHOCDF','quantity'],jsonIndex.recipe.totalDaily)/servingSize;
-    dailyCarbs = Math.round(dailyCarbs);
-
-    /*uses the ramda function to prevent crashing error
-    devines the values for each of the nutrients and calories and devides them by the serving size*/
+    let dailyNutrientsServing = getDailyNutrients(jsonIndex);
     
     $('#recipe-results').append(
       `<section class="recipe-group">
       
-      <section class="recipe-name">
-      <h2 class="header-Group">${jsonIndex.recipe.label}</h2>
-      <h2 class="serving-Group">(${servingSize} servings)</h2>
+        <section class="recipe-name">
+          <h2 class="header-Group">${jsonIndex.recipe.label}</h2>
+          <h2 class="serving-Group">(${dailyNutrientsServing[0]} servings)</h2>
         </section>
 
-      <section class="recipe-section ${i}">
-      <img src="${jsonIndex.recipe.image}" class="food-image">
+        <section class="recipe-section ${i}">
+          <img src="${jsonIndex.recipe.image}" class="food-image">
         </section>
 
-      <section class="nutrient-section">
-        <section class="recipe-header">
-          <h2 class="header-Group">Nutrients</h2>
-          <h2 class="serving-Group"> (per serving)</h2>
+        <section class="nutrient-section">
+          <section class="recipe-header">
+            <h2 class="header-Group">Nutrients</h2>
+            <h2 class="serving-Group"> (per serving)</h2>
+          </section>
+
+          <ul class="nutrient-list">
+          <li class="nutrient-group-one">${dailyNutrientsServing[1]} calories</li>
+          <li class="nutrient-group-two">${dailyNutrientsServing[2]}% of your daily Fat</li>
+          <li class="nutrient-group-one">${dailyNutrientsServing[3]}% of your daily Fiber</li>
+          <li class="nutrient-group-two">${dailyNutrientsServing[4]}% of your daily Protein</li>
+          <li class="nutrient-group-one">${dailyNutrientsServing[5]}% of your daily Cholesterol</li>
+          <li class="nutrient-group-two">${dailyNutrientsServing[6]}% of your daily Sodium</li>
+          <li class="nutrient-group-one">${dailyNutrientsServing[7]}% of your daily Calcium</li>
+          <li class="nutrient-group-two">${dailyNutrientsServing[8]}% of your daily Iron</li>
+          <li class="nutrient-group-one">${dailyNutrientsServing[9]}% of your daily Carbs</li></ul>
         </section>
 
-        <ul class="nutrient-list">
-        <li class="nutrient-group-one">${caloriesPerServing} calories</li>
-        <li class="nutrient-group-two">${dailyFat}% of your daily Fat</li>
-        <li class="nutrient-group-one">${dailyFiber}% of your daily Fiber</li>
-        <li class="nutrient-group-two">${dailyProtein}% of your daily Protein</li>
-        <li class="nutrient-group-one">${dailyCholesterol}% of your daily Cholesterol</li>
-        <li class="nutrient-group-two">${dailySodium}% of your daily Sodium</li>
-        <li class="nutrient-group-one">${dailyCalcium}% of your daily Calcium</li>
-        <li class="nutrient-group-two">${dailyIron}% of your daily Iron</li>
-        <li class="nutrient-group-one">${dailyCarbs}% of your daily Carbs</li></ul>
-      </section>
-
-      <section class="ingredient-section">
-        <section class="recipe-header">
-          <h2 class="recipe-info-header">Ingredients</h2>
+        <section class="ingredient-section">
+          <section class="recipe-header">
+            <h2 class="recipe-info-header">Ingredients</h2>
+          </section>
+          
+          <ul id="recipe-${i}"></ul>
         </section>
-        
-        <ul id="recipe-${i}"></ul>
-      </section>
 
-      <section class="link-section">
-      <a href=${jsonIndex.recipe.url} target="_blank">Find the cooking instructions here</a>
-      </section>
+        <section class="link-section">
+          <a href=${jsonIndex.recipe.url} target="_blank">Find the cooking instructions here</a>
+        </section>
       </section>`
       //generates the html for the nutrients section
     )
     getIngredients(responseJson,i);
   }
-  }
+}
+
+
 
 function getIngredients(responseJson,i) {
     const ingredientLength = responseJson.hits[i].recipe.ingredientLines.length;
@@ -217,13 +214,9 @@ function watchForm(fromVariable, toVariable) {
         alergyBooleanArray.push(alergyBoolean);
     }
     //goes through all the check boxes and if they are checked, append true, else false
-    document.getElementById('next-button').style.display = 'block';
     getInitialRecipeList(foodTerm, alergyBooleanArray, fromVariable, toVariable);
     navigationButtons(foodTerm, alergyBooleanArray, fromVariable, toVariable);
-    //shows the navigation buttons and runs the API call function
-
-    
-    
+    //shows the navigation buttons and runs the API call function      
   });
 }
 
